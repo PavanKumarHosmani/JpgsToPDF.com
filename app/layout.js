b86e51681314e85c903d83101ac41a0fb9bab342
pavanhosmani <PavanHosmani>
2025-10-03 01:04:42 +0530

    updated the robots.txt

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,9 @@
 // app/layout.js
-import "./globals.css";
+import "./styles/globals.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import GoogleAnalytics from "../components/GoogleAnalytics";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata = {
   title: "JPG to PDF Converter | Free JPG, PNG & WebP to PDF Converter",
